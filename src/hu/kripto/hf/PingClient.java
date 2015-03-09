8d3cf767595a84a5db27b9028d6141aa8e09b68b
bendaf <bendaf92@gmail.com>
2015-03-09 18:48:15 +0100

    Add first and third step to ping client

--- src/hu/kripto/hf/PingClient.java
+++ src/hu/kripto/hf/PingClient.java
@@ -2,16 +2,43 @@ package hu.kripto.hf;
 
 
 
-import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException; // Ha a kommunikacioban valami balul sul el
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException; // Ha rossz cimre probalunk csatlakozni
+import java.util.Random;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class PingClient implements Runnable {
+	protected Socket clientSocket;
+	private Integer myModulus;
+	private long myResult;
+	private DH dh;
+	
 	public PingClient() throws UnknownHostException, IOException {
 		clientSocket = new Socket(InetAddress.getLocalHost(), Server.PORT_NUMBER);
 	}
@@ -21,40 +48,122 @@ public class PingClient implements Runnable {
 	}
 	
 	public void run() {
+
 		try {
+			
 			DataInputStream serverInput = new DataInputStream(clientSocket.getInputStream());
-			PrintWriter serverOutput = new PrintWriter(clientSocket.getOutputStream());
-
-			for (int i = 0; i < 1; ++i) {
-				long startTime = System.currentTimeMillis();
-				serverOutput.print("PING\r\n");
-				serverOutput.flush(); // Buffer uritese
-//				System.out.println(String.valueOf(serverInput.readInt()));
-				byte[] b = new byte[serverInput.readInt()];
-				serverInput.read(b);
-				System.out.println(b);
-				
-//				if (serverInput.readLine().equals("PONG")) {
-//					long endTime = System.currentTimeMillis();
-//					System.out.println("Az uzenet visszaert " + new Long(endTime - startTime).toString() + " ms alatt");
-//					try {
-//						Thread.sleep(400);
-//					} catch (InterruptedException e) {
-//						System.out.println("Interrupted while sleeping, shutting down...");
-//						clientSocket.close(); // Minden lehetseges ponton le kell zarni a socket-okat
-//						return;
-//					}
-//				}
-			}
-
-			serverOutput.print("QUIT\r\n");
+			DataOutputStream serverOutput = new DataOutputStream(clientSocket.getOutputStream());
+			
+			
+			byte[] firstStep = firstStep();
+			serverOutput.writeInt(firstStep.length);
+			serverOutput.write(firstStep);
 			serverOutput.flush();
-
+			
+			byte[] b = new byte[serverInput.readInt()];
+			serverInput.read(b);
+			
+			try {
+				Document d = obtenerDocumentDeByte(b);
+				d.getDocumentElement().normalize();
+				
+				NodeList nList = d.getElementsByTagName("dh");
+				Node nNode = nList.item(0);
+				NodeList ghChildren = nNode.getChildNodes();
+				int step = -1;
+				for (int temp = 0; temp < ghChildren.getLength(); temp++) {
+					 
+					Node node = ghChildren.item(temp);
+			 
+					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+						node.getChildNodes();
+						Element eElement = (Element) node;
+						if(eElement.getNodeName().equals("step")){
+							step = Integer.parseInt(eElement.getTextContent());
+						}else if (eElement.getNodeName().equals("myresult")) {
+							myResult = Long.parseLong(eElement.getTextContent());
+						}
+					}
+				}
+				if(step == 2){
+					try {
+						dh = new DH(2, myModulus);
+						byte[] thirdStep = thirdStep(Long.toHexString(
+						dh.createInterKey()));
+						serverOutput.writeInt(thirdStep.length);
+						serverOutput.write(thirdStep);
+						serverOutput.flush();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (ParserConfigurationException | SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+//			System.out.println(firstStep.length);
+			
 			// Itt se felejtsetek lezarni a kapcsolatot!
 			clientSocket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private byte[] thirdStep(String hexString) {
+		try{
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+	
+			// root elements
+			Document doc = docBuilder.newDocument();
+			Element rootElement = doc.createElement("dh");
+			doc.appendChild(rootElement);
+			
+			Element record = doc.createElement("step");
+			record.setTextContent("3");
+			rootElement.appendChild(record);
+			
+			Element modulus = doc.createElement("myresult");
+			modulus.setTextContent(hexString);
+			rootElement.appendChild(modulus);
+			
+			// write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("outexample.xml"));
+	
+			// Output to console for testing
+			// StreamResult result = new StreamResult(System.out);
+	
+			transformer.transform(source, result);
+		} catch (DOMException e) {
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+	
+		byte[] fileData = null;
+		try {
+			File file = new File("outexample.xml");
+			fileData = new byte[(int) file.length()];
+			FileInputStream in = new FileInputStream(file);
+			in.read(fileData);
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return fileData;
 	}
 
 	public static void main(String[] args) {
@@ -66,6 +175,73 @@ public class PingClient implements Runnable {
 	}
 	
 	// Csatlakozunk a sajat gepunkon futo szerverhez. A sajat gepunk hostneve localhost, ip cime 127.0.0.1.
-	protected Socket clientSocket;
+	private byte[] firstStep(){
+		try{
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 	
+			// root elements
+			Document doc = docBuilder.newDocument();
+			Element rootElement = doc.createElement("dh");
+			doc.appendChild(rootElement);
+			
+			Element record = doc.createElement("step");
+			record.setTextContent("1");
+			rootElement.appendChild(record);
+			
+			Element modulus = doc.createElement("modulus");
+			modulus.setTextContent(Integer.toString(chooseModulus()));
+			rootElement.appendChild(modulus);
+			
+			// write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("outexample.xml"));
+	
+			// Output to console for testing
+			// StreamResult result = new StreamResult(System.out);
+	
+			transformer.transform(source, result);
+		} catch (DOMException e) {
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+	
+		byte[] fileData = null;
+		try {
+			File file = new File("outexample.xml");
+			fileData = new byte[(int) file.length()];
+			FileInputStream in = new FileInputStream(file);
+			in.read(fileData);
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return fileData;
+	}
+
+	private Document obtenerDocumentDeByte(byte[] documentoXml) throws ParserConfigurationException, SAXException, IOException  {
+	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    factory.setNamespaceAware(true);
+	    DocumentBuilder builder = factory.newDocumentBuilder();
+	    return builder.parse(new ByteArrayInputStream(documentoXml));
+	}
+	
+	private int chooseModulus() {
+		Random r = new Random();
+		myModulus = CONSTANTS.MOD_NUMS[r.nextInt(CONSTANTS.MOD_NUMS.length)]; 
+		
+		return myModulus;
+	}
 }
